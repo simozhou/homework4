7e88bdaac497f04dea09e2470edcc4ee10dd23d4
Simone Procaccia <32101556+simozhou@users.noreply.github.com>
2017-12-22 22:55:37 +0100

    Added graphyte module and similarity module for graph analisys and similarity algos

---
+++ graphyte.py
@@ -0,0 +1,17 @@
+"""A module containing all the functions we want to create to facilitate the graphing procedure
+of the dictionary of words. Depends on the IOData module.
+"""
+
+
+def subgraph_length(graph, lenght=None):
+    """"Purpose: equivalent words; returns a subgraph of words with equal length from the general dictGraph"""
+    pass
+
+
+def graph_build(func, array):
+    """
+    Purpose: Generalized equivalent words; returns a  non-directed graph of words
+    with edges representing function func which will return 1. if no func is passed, use similarity.edit_distance
+    as default.
+    """
+    pass
