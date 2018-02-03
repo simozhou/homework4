6a7ed665b0f1bbfdf1dea37c21b51dd898ba1de1
Simone <simone.procaccia@gmail.com>
2018-02-03 21:28:59 +0100

    Simone \\ 03/02/18 \\ Optimized grapher function, commented eveerything and added some error-catcher to equivalent_words.

--- equivalent_words.py
+++ equivalent_words.py
@@ -1,31 +1,44 @@
 import networkx as nx
 from collections import defaultdict
-from itertools import groupby
+from itertools import groupby, combinations
 import pickle
 
+
 def grapher(wordFile):
-    d = {}
+    """
+    Input: a .txt file containing you dictionary (separator \n)
+    Output: a graph of all the words connected by edit distance value = 1
+
+    The basic concept is "bucketing": dividing words with respect to some pseudo-words (have a placeholder instead
+    of a letter each time). after this process is done, we have buckets of words which needs to get connected within the
+    graph and by takiing the combination of the values in each bucket we have our graph cooked. This bad boy of a
+    function allow us to solve the problem of the graph building in a quasi-linear time complexity ( O((2s+1)^2*n*b^2)
+    s = avg word len, n = dict size, b = avg size of buckets, both b and s are relatively small with respect to n,
+    so they can be considered as constant and cut off).
+    """
+    dictionary = {}
     g = nx.Graph()
     wfile = open(wordFile, 'r')
     # create buckets of words that differ by one letter
     for line in wfile:
         word = line[:-1]
         for bucket in word_cooker(word):
-            if bucket in d:
-                if word in d[bucket]:
-                    d[bucket].append(word)
+            if bucket in dictionary:
+                if word not in dictionary[bucket]:
+                    dictionary[bucket].append(word)
             else:
-                d[bucket] = [word]
+                dictionary[bucket] = [word]
     # add vertices and edges for words in the same bucket
-    for bucket in d.keys():
-        for word1 in d[bucket]:
-            for word2 in d[bucket]:
-                g.add_edge(word1, word2)
+    for bucket in dictionary.keys():
+        g.add_edges_from(combinations(dictionary[bucket], 2))
     return g
 
 
 def data_baker_len(filename="words_alpha.txt"):
-    """function that takes in a text file of words and bae them into a python list + dict them according to sizes"""
+    """
+    function that takes in a text file of words and bake it into a dict of struct:
+    ---> {len(word): [word1, word2, etc]}
+    """
     with open(filename) as f:
         words = f.read().split()
 
@@ -37,6 +50,7 @@ def data_baker_len(filename="words_alpha.txt"):
 
 
 def word_cooker(word):
+    """actual creator of the buckets names"""
     yield "_" + word
     for i in range(len(word)):
         yield word[:i] + "_" + word[i + 1:]
@@ -44,6 +58,14 @@ def word_cooker(word):
 
 
 def equivalent_words(start, end, gameplay="general"):
+    """
+    the actual game function. Takes in a start and end word an a gameplay (simple or general).
+    return a string with the path from start to end.
+
+    Raises ValueError if in simple mode words of different length are plugged in.
+
+    """
+    # the graph we're using has been previously pickled to avoid those seconds of waiting for the graph get built
     graph = nx.read_gpickle("graph.pickle")
     dict_len = pickle.load(open("len_dict.pickle", 'rb'))
     if gameplay == "simple":
@@ -55,8 +77,18 @@ def equivalent_words(start, end, gameplay="general"):
         result_list = nx.astar_path(graph, start, end)
     except nx.NetworkXNoPath:
         return "No path found between %s and %s" % (start, end)
+    except nx.NodeNotFound:
+        return "One of the two words doesn't exist or is not in this graph. Try to be serious for once."
     result_string = "Path:\n" + ' -> '.join(result_list)
     return result_string
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    # just some raw benchmark for the grapher function
+    import time
+
+    start = time.time()
+    graph = grapher("words_alpha.txt")
+    end = time.time() - start
+    print(end)
     print(equivalent_words("head", "tail", "simple"))
